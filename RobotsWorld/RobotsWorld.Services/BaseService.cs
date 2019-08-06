37516cb1ce6f2d6af3f5675b5c6151a15873b4ec
vallecbg <valioplayofficial@gmail.com>
2019-08-06 14:46:02 +0300

    Added base service

---
+++ RobotsWorld/RobotsWorld.Services/BaseService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using Microsoft.AspNetCore.Identity;
+using RobotsWorld.Data;
+using RobotsWorld.Models;
+
+namespace RobotsWorld.Services
+{
+    public abstract class BaseService
+    {
+        protected BaseService(UserManager<User> userManager,
+            RobotsWorldContext context,
+            IMapper mapper)
+        {
+            this.UserManager = userManager;
+
+            this.Context = context;
+            this.Mapper = mapper;
+        }
+
+        protected IMapper Mapper { get; }
+
+        protected RobotsWorldContext Context { get; }
+
+        protected UserManager<User> UserManager { get; }
+    }
+}
