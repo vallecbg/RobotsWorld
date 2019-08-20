3aafdd651cf3cc9368c5ebc66481e68558525733
vallecbg <valioplayofficial@gmail.com>
2019-08-20 16:15:25 +0300

    Added Robot Service Tests covering 96%

---
+++ RobotsWorld/RobotsWorld.Tests/BaseService.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using RobotsWorld.Data;
+using RobotsWorld.Models;
+using RobotsWorld.Services;
+using RobotsWorld.Services.Contracts;
+using RobotsWorld.Services.Mapper;
+using RobotsWorld.Web.HelperMethods;
+
+namespace RobotsWorld.Tests
+{
+    [TestFixture]
+    public class BaseService
+    {
+        protected IServiceProvider Provider { get; set; }
+
+        protected RobotsWorldContext Context { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Mapper.Reset();
+            Mapper.Initialize(x => { x.AddProfile<MapperProfile>(); });
+            var services = SetServices();
+            this.Provider = services.BuildServiceProvider();
+            this.Context = this.Provider.GetRequiredService<RobotsWorldContext>();
+            SetScoppedServiceProvider();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.Context.Database.EnsureDeleted();
+        }
+
+        private void SetScoppedServiceProvider()
+        {
+            var httpContext = this.Provider.GetService<IHttpContextAccessor>();
+            httpContext.HttpContext.RequestServices = this.Provider.CreateScope().ServiceProvider;
+        }
+
+        private ServiceCollection SetServices()
+        {
+            var services = new ServiceCollection();
+
+            services.AddDbContext<RobotsWorldContext>(
+                cfg => cfg.UseInMemoryDatabase(Guid.NewGuid().ToString()));
+
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IRobotService, RobotService>();
+            services.AddScoped<IAssemblyService, AssemblyService>();
+            services.AddScoped<ISubAssemblyService, SubAssemblyService>();
+            services.AddScoped<IPartService, PartService>();
+            services.AddScoped<IVendorService, VendorService>();
+            services.AddScoped<IDeliveryService, DeliveryService>();
+            services.AddScoped<ITransportTypeService, TransportTypeService>();
+            services.AddScoped<IAdminService, AdminService>();
+
+            services.AddIdentity<User, IdentityRole>(opt =>
+            {
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequiredLength = 6;
+                opt.Password.RequiredUniqueChars = 0;
+            })
+                .AddEntityFrameworkStores<RobotsWorldContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAutoMapper();
+
+            var context = new DefaultHttpContext();
+
+            services.AddSingleton<IHttpContextAccessor>(
+                new HttpContextAccessor()
+                {
+                    HttpContext = context
+                });
+
+            return services;
+        }
+    }
+}
---
+++ RobotsWorld/RobotsWorld.Tests/RobotServiceTests.cs
@@ -0,0 +1,397 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using RobotsWorld.Models;
+using RobotsWorld.Services.Contracts;
+using RobotsWorld.ViewModels.InputModels.Robots;
+using FluentAssertions;
+using RobotsWorld.Services.Constants;
+using RobotsWorld.ViewModels.OutputModels.Robots;
+
+namespace RobotsWorld.Tests
+{
+    [TestFixture]
+    public class RobotServiceTests : BaseService
+    {
+        protected IRobotService robotService => this.Provider.GetRequiredService<IRobotService>();
+        protected UserManager<User> userManager => this.Provider.GetRequiredService<UserManager<User>>();
+        protected RoleManager<IdentityRole> roleManager => this.Provider.GetRequiredService<RoleManager<IdentityRole>>();
+
+        [Test]
+        public void CreateRobot_With_No_Image_Should_Succeed()
+        {
+            var user = new User()
+            {
+                Id = "1",
+                Name = "Gosho",
+                UserName = "gosho123"
+            };
+
+            userManager.CreateAsync(user).GetAwaiter();
+            this.Context.SaveChanges();
+
+            var robotInputModel = new RobotInputModel
+            {
+                User = user.UserName,
+                Image = null,
+                Axes = 2,
+                Name = "myRobot",
+                SerialNumber = "24242"
+            };
+
+            var result = this.robotService.CreateRobot(robotInputModel).GetAwaiter().GetResult();
+
+            var robot = this.Context.Robots.FirstOrDefault();
+
+            result.Should().NotBeNull();
+            robot.Should().NotBeNull()
+                .And.Subject.Should().BeEquivalentTo(new
+                {
+                    Id = result,
+                    ImageUrl = GlobalConstants.NoImageAvailableUrl,
+                    Name = robot.Name
+                }, options => options.ExcludingMissingMembers());
+        }
+
+        [Test]
+        public void DeleteRobot_Should_Succeed()
+        {
+            var admin = new User()
+            {
+                Id = "123",
+                Name = "Papuncho Kunchev",
+                UserName = "admin"
+            };
+            var user = new User()
+            {
+                Id = "321",
+                Name = "Kuncho Papunchev",
+                UserName = "author"
+            };
+
+            var robot = new Robot()
+            {
+                Id = "1",
+                UserId = user.Id,
+                Axes = 2,
+                Name = "myRobot",
+                SerialNumber = "2323"
+            };
+
+            userManager.CreateAsync(admin).GetAwaiter();
+            userManager.CreateAsync(user).GetAwaiter();
+
+            var role = new IdentityRole { Name = "admin" };
+            roleManager.CreateAsync(role).GetAwaiter();
+            userManager.AddToRoleAsync(admin, role.Name).GetAwaiter();
+
+            this.Context.Robots.Add(robot);
+            this.Context.SaveChanges();
+
+            robotService.DeleteRobot(robot.Id, admin.UserName);
+
+            var result = this.Context.Robots.FirstOrDefault();
+
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public void DeleteRobot_Should_Succeed_With_All_Of_The_Nested_Classes()
+        {
+            var admin = new User()
+            {
+                Id = "123",
+                Name = "Papuncho Kunchev",
+                UserName = "admin"
+            };
+            var user = new User()
+            {
+                Id = "321",
+                Name = "Kuncho Papunchev",
+                UserName = "author"
+            };
+
+            var robot = new Robot()
+            {
+                Id = "1",
+                UserId = user.Id,
+                Axes = 2,
+                Name = "myRobot",
+                SerialNumber = "2323"
+            };
+
+            var assembly = new Assembly()
+            {
+                Id="1",
+                RobotId = robot.Id
+            };
+
+            var subAssembly = new SubAssembly()
+            {
+                AssemblyId = assembly.Id,
+                Id = "1",
+                Quantity = 1,
+                Weight = 1,
+                Name = "Motherboard"
+            };
+
+            var vendor = new Vendor()
+            {
+                Id="1",
+                Name = "IBM"
+            };
+
+            var part = new Part()
+            {
+                Id="1",
+                Price = 2.50m,
+                Quantity = 2,
+                SubAssemblyId = subAssembly.Id,
+                VendorId = vendor.Id
+            };
+
+            userManager.CreateAsync(admin).GetAwaiter();
+            userManager.CreateAsync(user).GetAwaiter();
+
+            var role = new IdentityRole { Name = "admin" };
+            roleManager.CreateAsync(role).GetAwaiter();
+            userManager.AddToRoleAsync(admin, role.Name).GetAwaiter();
+
+            this.Context.Robots.Add(robot);
+            this.Context.Assemblies.Add(assembly);
+            this.Context.SubAssemblies.Add(subAssembly);
+            this.Context.Parts.Add(part);
+            this.Context.Vendors.Add(vendor);
+            this.Context.SaveChanges();
+
+            robotService.DeleteRobot(robot.Id, admin.UserName);
+
+            var result = this.Context.Robots.FirstOrDefault();
+
+            result.Should().BeNull();
+        }
+
+        [Test]
+        public void DeleteRobot_Should_Throw_Error_NoRights()
+        {
+            var user1 = new User()
+            {
+                Id = "123",
+                Name = "Papuncho Kunchev",
+                UserName = "user1"
+            };
+            var user2 = new User()
+            {
+                Id = "321",
+                Name = "Kuncho Papunchev",
+                UserName = "user2"
+            };
+
+            var robot = new Robot()
+            {
+                Id = "1",
+                UserId = user2.Id,
+                Axes = 2,
+                Name = "myRobot",
+                SerialNumber = "2323"
+            };
+
+            userManager.CreateAsync(user1).GetAwaiter();
+            userManager.CreateAsync(user2).GetAwaiter();
+
+            this.Context.Robots.Add(robot);
+            this.Context.SaveChanges();
+
+            Action act = () => robotService.DeleteRobot(robot.Id, user1.UserName);
+
+            var message = GlobalConstants.UserHasNoRights;
+
+            act.Should().Throw<InvalidOperationException>().WithMessage(message);
+        }
+
+        [Test]
+        public void EditRobot_Should_Succeed()
+        {
+            var robot = new Robot()
+            {
+                Id = "1",
+                UserId = "123",
+                Axes = 2,
+                Name = "myRobot",
+                SerialNumber = "2323"
+            };
+
+            this.Context.Robots.Add(robot);
+            this.Context.SaveChanges();
+
+            var robotEditModel = new RobotEditModel()
+            {
+                Id = "1",
+                Name = "changed",
+                Axes = 4,
+                SerialNumber = "3333",
+                Image = null
+            };
+
+            this.robotService.EditRobot(robotEditModel);
+
+            var result = this.Context.Robots.Find(robot.Id);
+
+            result.Should().NotBeNull()
+                .And.Subject.As<Robot>()
+                .Name.Should().BeEquivalentTo(robotEditModel.Name);
+        }
+
+        
+
+        [Test]
+        public void GetUserRobots_Should_Succeed()
+        {
+            var user = new User()
+            {
+                Id = "123",
+                Name = "Kuncho Papunchev",
+                UserName = "author"
+            };
+
+            var robots = new[]
+            {
+                new Robot()
+                {
+                    Id = "1",
+                    Axes = 2,
+                    Name = "asdad",
+                    SerialNumber = "2323223",
+                    UserId = user.Id
+                },
+                new Robot()
+                {
+                    Id = "2",
+                    Axes = 5,
+                    Name = "eqweqw",
+                    SerialNumber = "2222",
+                    UserId = "55"
+                },
+            };
+
+            userManager.CreateAsync(user).GetAwaiter();
+            this.Context.Robots.AddRange(robots);
+            this.Context.SaveChanges();
+
+            var result = this.robotService.GetUserRobots(user.Id);
+
+            result.Should().NotBeNull()
+                .And.BeOfType<List<RobotOutputModel>>()
+                .And.HaveCount(1);
+        }
+
+        [Test]
+        public void GetRobotDetails_Should_Succeed()
+        {
+            var robot = new Robot()
+            {
+                Id = "1",
+                UserId = "123",
+                Axes = 2,
+                Name = "myRobot",
+                SerialNumber = "2323"
+            };
+
+            this.Context.Robots.Add(robot);
+            this.Context.SaveChanges();
+
+            var result = this.robotService.GetRobotDetails(robot.Id);
+
+            result.Should().BeOfType<RobotOutputModel>()
+                .And.Should().NotBeNull();
+        }
+
+        [Test]
+        public void GetRobotToEdit_Should_Succeed()
+        {
+            var robot = new Robot()
+            {
+                Id = "1",
+                UserId = "123",
+                Axes = 2,
+                Name = "myRobot",
+                SerialNumber = "2323"
+            };
+
+            this.Context.Robots.Add(robot);
+            this.Context.SaveChanges();
+
+            var result = this.robotService.GetRobotToEdit(robot.Id);
+
+            result.Should().NotBeNull()
+                .And.BeOfType<RobotEditModel>();
+        }
+
+        [Test]
+        public void GetAssembliesCount_Should_Succeed()
+        {
+            var robot = new Robot()
+            {
+                Id = "1",
+                UserId = "123",
+                Axes = 2,
+                Name = "myRobot",
+                SerialNumber = "2323",
+                Assembly = new Assembly()
+                {
+                    Id="1"
+                }
+            };
+
+            this.Context.Robots.Add(robot);
+            this.Context.SaveChanges();
+
+            var result = this.robotService.GetAssembliesCount(robot.Id);
+
+            result.Should().Be(1);
+        }
+        
+        [Test]
+        public void CheckUserIsOwnerOfRobot_Should_Succeed()
+        {
+            var user = new User()
+            {
+                Id = "123",
+                Name = "Kuncho Papunchev",
+                UserName = "author"
+            };
+
+            var robots = new[]
+            {
+                new Robot()
+                {
+                    Id = "1",
+                    Axes = 2,
+                    Name = "asdad",
+                    SerialNumber = "2323223",
+                    UserId = user.Id
+                },
+                new Robot()
+                {
+                    Id = "2",
+                    Axes = 5,
+                    Name = "eqweqw",
+                    SerialNumber = "2222",
+                    UserId = "55"
+                },
+            };
+
+            userManager.CreateAsync(user).GetAwaiter();
+            this.Context.Robots.AddRange(robots);
+            this.Context.SaveChanges();
+
+            var result = this.robotService.CheckUserIsOwnerOfRobot(user.Id, robots[0].Id);
+
+            result.Should().BeTrue();
+        }
+    }
+}
