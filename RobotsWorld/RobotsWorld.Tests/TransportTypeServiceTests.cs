ddcadcd7549740d0e4050fbcdb4c3f1b45a5fa66
vallecbg <valioplayofficial@gmail.com>
2019-08-21 17:46:43 +0300

    Added Transport Type Service Tests Covering 100%

---
+++ RobotsWorld/RobotsWorld.Tests/TransportTypeServiceTests.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using RobotsWorld.Models;
+using RobotsWorld.Services.Contracts;
+
+namespace RobotsWorld.Tests
+{
+    [TestFixture]
+    public class TransportTypeServiceTests : BaseService
+    {
+        protected ITransportTypeService transportTypeService => this.Provider.GetRequiredService<ITransportTypeService>();
+
+        [Test]
+        public void GetAllTransportTypes_Should_Succeed()
+        {
+            var transportTypes = new[]
+            {
+                new TransportType()
+                {
+                    Name = "transport1"
+                },
+                new TransportType()
+                {
+                    Name = "transport2"
+                },
+                new TransportType()
+                {
+                    Name = "transport3"
+                },
+            };
+
+            this.Context.TransportTypes.AddRange(transportTypes);
+            this.Context.SaveChanges();
+
+            var result = this.transportTypeService.GetAllTransportTypes().First();
+
+            result.Should().NotBeNullOrWhiteSpace()
+                .And.Subject.Should().Be(transportTypes[0].Name);
+        }
+
+        [Test]
+        public void GetTransportTypeByName_Should_Succeed()
+        {
+            var transportTypes = new[]
+            {
+                new TransportType()
+                {
+                    Name = "transport1"
+                },
+                new TransportType()
+                {
+                    Name = "transport2"
+                },
+                new TransportType()
+                {
+                    Name = "transport3"
+                },
+            };
+
+            this.Context.TransportTypes.AddRange(transportTypes);
+            this.Context.SaveChanges();
+
+            var result = this.transportTypeService.GetTransportTypeByName(transportTypes[0].Name);
+
+            result.Should().NotBeNull()
+                .And.Subject.As<TransportType>()
+                .Name.Should().Be(transportTypes[0].Name);
+        }
+    }
+}
